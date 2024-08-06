6846107075b6f8025188e39a27f7abee7cf53927
Yetnayet Lakew <127979977+infangle@users.noreply.github.com>
2024-08-06 19:05:37 +0000

    project setup

---
+++ task-8/app/[components]/Navigation.jsx
@@ -0,0 +1,9 @@
+// top navigation bar
+
+import React from "react";
+
+const Navigation = () => {
+  return <h1>navigation</h1>;
+};
+
+export default Navigation;
---
+++ task-8/app/member/page.jsx
@@ -0,0 +1,9 @@
+// for only authenticated members
+"use client";
+import React from "react";
+
+const Member = () => {
+  return <div>Member Session </div>;
+};
+
+export default Member;
